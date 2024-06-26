c033c53fcce5b37db2b321df6b028c3abb528b2b
Oliver Palnau <oliverpalnau@gmail.com>
2024-06-26 01:28:24 -0400

    ve

--- components/ServicesSectionAlternate.tsx
+++ components/ServicesSectionAlternate.tsx
@@ -80,7 +80,7 @@ export default function ServicesSectionAlternate() {
                 duration: 0.5,
                 ease: [0.4, 0.0, 0.2, 1],
               }}
-              className="text-4xl px-4 md:text-4xl lg:text-6xl font-bold text-neutral-700 dark:text-white max-w-full leading-relaxed lg:leading-snug text-center mx-auto"
+              className="text-2xl px-4 md:text-4xl lg:text-6xl font-bold text-neutral-700 dark:text-white max-w-full leading-relaxed lg:leading-snug text-center mx-auto mt-4 mb-4"
             >
               {highlightText(words)}
             </motion.h1>
